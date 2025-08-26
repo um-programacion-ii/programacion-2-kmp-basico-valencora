6f2f280abcbbb992d14b6f3d03a7ea3162dc1404
valencora <coratolovalen@gmail.com>
2025-08-26 18:13:35 -0300

    clase

--- composeApp/src/commonMain/kotlin/org/basic/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/basic/project/App.kt
@@ -4,15 +4,20 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -21,6 +26,54 @@ import kmpbasico.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 fun App() {
+    var name: String = ""
+
     MaterialTheme {
+        var name: String by remember { mutableStateOf("") }
+        var pass: String by remember { mutableStateOf("") }
+
+        Column(
+            modifier = Modifier
+                .background(color = MaterialTheme.colorScheme.primaryContainer)
+                .safeContentPadding()
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TextField(
+                value = name,
+                onValueChange = {
+                    name = it
+                }
+            )
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            TextField(
+                value = pass,
+                onValueChange = {
+                    pass = it
+                }
+            )
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            AnimatedVisibility(visible = name.isNotEmpty()) {
+                Text(
+                    text = "versión animada $name",
+                    fontSize = 24.sp
+                )
+            }
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            AnimatedVisibility(visible = pass.isNotEmpty()) {
+                Text(
+                    text = "versión animada $pass",
+                    fontSize = 24.sp
+                )
+            }
+
+
+        }
     }
 }
